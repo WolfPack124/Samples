f883e726722d8b661849adc5bf5d69ed873c41bf
Aswani Kumar Thiruvanghadan <aswani_kumar_t@yahoo.in>
2016-03-01 16:56:35 +0530

    Added files via upload

---
+++ BalancePara.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define N 1000
+
+int CheckPara(char String[N], int len)
+{
+    char Stack[N] = {0};
+    int top = -1;
+    for(int i = 0; i < len; i++) {
+        
+        if(String[i] == '{' || String[i] == '[' || String[i] == '(') {
+            Stack[++top] = String[i];
+        }
+        if(String[i] == ']') {
+            if(Stack[top] != '[') {
+                return 0;
+            }
+            top--;
+        }
+        if(String[i] == '}') {
+            if(Stack[top] != '{') {
+                return 0;
+            }
+            top--;
+        }
+        if(String[i] == ')') {
+            if(Stack[top] != '(') {
+                return 0;
+            }
+            top--;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    char String[N];
+    int result = 0;
+    printf("Enter the expression : ");
+    scanf(" %s", String);
+    result = CheckPara(String, strlen(String));
+    if(result) {
+        printf("Braces are balanced \n");
+    } else {
+        printf("Braces are not balanced \n");
+    }
+}
